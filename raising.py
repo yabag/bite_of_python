683df69774761ac539d6ea90588fffa3a409df7b
yabag <yabagratyan@mail.ru>
2022-05-26 10:49:46 +0300

    created files: pickling.py, try_except.py, raising.py, finally.py, using_with.py

---
+++ raising.py
@@ -0,0 +1,15 @@
+class ShortInputException(Exception):
+    def __init__(self, length, atleast):
+        Exception.__init__(self)
+        self.length = length
+        self.atleast = atleast
+
+try:
+    text = input('Введите что-нибудь --> ')
+    if len(text) < 3:
+        raise ShortInputException(len(text), 3)
+    # Здесь может происходить обычная работа
+except EOFError:
+    print('Ну зачем вы сделали мне EOF?')
+except ShortInputException as ex:
+    print('ShortInputException: Длина введённой строки -- {0}; ожидалась, как минимум, {1}'.format(ex.length, ex.atleast))
